73b48b073cd557882ac55166e5a127d08bcb3fa9
Mateusz <matyd356@gmail.com>
2020-12-12 21:40:04 +0100

    add flags to currency rates

--- src/components/Rates.js
+++ src/components/Rates.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import '../styles/Rates.scss'
+import 'currency-flags/dist/currency-flags.css'
 
 const Rates = ({ nbpData }) => {
 
   return (
     <main className='Rates'>
       {nbpData?.rates?.map(item => {
-        if (item.code === 'PLN') {
+        console.log(item.code.toLowerCase());
+        if (item.code === 'PLN' || item.code === 'XDR') {
           return null;
         }
-        return <div className='Rates-item'>
-          <p className='Rates-full-name'>{item.currency}</p>
-          <p className='Rates-rate'>1 {item.code} = {item.mid.toFixed(4)} PLN</p>
+        return <div className='Rates-item' key={item.code}>
+          <div className={`currency-flag currency-flag-xl currency-flag-${item.code.toLowerCase()}`}></div>
+          <div className='Rates-item-desc'>
+            <p className='Rates-full-name'>{item.currency}</p>
+            <p className='Rates-rate'>1 {item.code} = {item.mid.toFixed(4)} PLN</p>
+          </div>
         </div>
       })}
     </main>
